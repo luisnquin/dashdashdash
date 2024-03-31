612ec6dcaa3e21da0654bb0e882f3a39160f17ea
Luis Quiñones <lpaandres2020@gmail.com>
2024-03-30 22:14:51 -0500

    feat: dev shell with turso and redis

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = import nixpkgs {
+            config = {
+              allowBroken = false;
+              allowUnfree = true;
+            };
+            inherit system;
+          };
+        in {
+          defaultPackage = pkgs.hello;
+
+          devShells.default = pkgs.mkShell {
+            inherit system;
+
+            buildInputs = with pkgs; [
+              turso-cli
+              redis
+            ];
+          };
+        }
+      );
+}
