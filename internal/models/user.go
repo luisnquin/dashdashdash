f25a821aec557c48d586d0d44fd68f3685c481c7
Luis Quiñones <lpaandres2020@gmail.com>
2024-03-31 19:41:07 -0500

    fix(user): replaced *string by string in password field

--- internal/models/user.go
+++ internal/models/user.go
@@ -7,7 +7,7 @@ import (
 type User struct {
 	ID        int       `db:"id"         json:"id"`
 	Username  string    `db:"username"   json:"username"`
-	Password  *string   `db:"password"   json:"password,omitempty"`
+	Password  string    `db:"password"   json:"password,omitempty"`
 	Email     string    `db:"email"      json:"email"`
 	CreatedAt time.Time `db:"created_at" json:"createdAt"`
 }
